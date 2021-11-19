cd150c4a112fc9d01377a9b2000a00f8f22aba9f
Ben Rickers <bennyrickers@gmail.com>
2021-11-18 17:33:09 -0700

    changed the models files in each of the apps. added what I think the tables will be.

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from game.models import Pokemon
+from tails.models import Person
+
+# at the end of the game the user will be displayed with 
+# the pokemon they caught and whether they caught them all
+# it will display the pokemon they caught and the pokemon they didn't catch
 
 # Create your models here.
+class EndGame(models.Model):
+    user_name = models.ForeignKey(Person, on_delete=models.CASCADE)
+    poke_caught = models.CharField(max_length=30) 
+    poke_missed = models.CharField(max_length=30)
+    poke_pic = models.ImageField(Pokemon, upload_to='uploads/')
+    poke_id = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
--- game/models.py
+++ game/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+# By default pokemon are wild.
+# When a user successfully catches a pokemon, wild_pokemon will be changed to false.
+# the admin will have control over editing default pokemon in Wild_Pokemon
+
+# a pokemon will be caught when the user guesses a number between the high_num and low_num
+
+class Pokemon(models.Model):
+    poke_id= models.IntegerField(primary_key=True)
+    poke_name = models.CharField(max_length=30)
+    poke_pic = models.ImageField(upload_to='uploads/')
+    low_num = models.IntegerField()
+    high_num = models.IntegerField()
+    wild_pokemon = models.BooleanField(default=True) #if updated to false, pokemon has been caught
+    
+
+
+
+
+
